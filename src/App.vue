f16aa85154f6ec761c5a9ffe20bfd215dd405caa
MichelleNF <a3521110604@alumno.uttehuacan.edu.mx>
2022-10-10 23:19:20 -0500

    First version

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+ <template>
+    <div class="container mt-3"> 
+        <Header/>
+        <Formulario/>
+    </div>
+</template>
+
+ <script>
+    import Header from "./components/Header.vue";
+    import Formulario from "./components/Formulario.vue";
+
+    export default {
+        components: {
+            Header,
+            Formulario, 
+        },
+    };
+ </script>
